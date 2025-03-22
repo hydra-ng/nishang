c048ff98d1d7d3bb790f56e295d2929914ec3776
joe <joe@rstrike.my.id>
2025-03-22 13:24:32 +0000

    Import NISHANG packages from BlackTrack repos

---
+++ debian/upstream/metadata.ex
@@ -0,0 +1,10 @@
+# Example file for upstream/metadata.
+# See https://wiki.debian.org/UpstreamMetadata for more info/fields.
+# Below an example based on a github project.
+
+# Bug-Database: https://github.com/<user>/nishang/issues
+# Bug-Submit: https://github.com/<user>/nishang/issues/new
+# Changelog: https://github.com/<user>/nishang/blob/master/CHANGES
+# Documentation: https://github.com/<user>/nishang/wiki
+# Repository-Browse: https://github.com/<user>/nishang
+# Repository: https://github.com/<user>/nishang.git
